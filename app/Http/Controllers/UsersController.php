b0bce821a1e90631037dde03739ae932c03e3c58
masaki.ogihara <hamuzo5488811@gmail.com>
2021-06-18 17:21:26 +0000

    post

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -21,8 +21,15 @@ class UsersController extends Controller
     {
         $user = User::findOrFail($id);
         
+        // 関係するモデルの件数をロード
+        $user->loadRelationshipCounts();
+
+        // ユーザの投稿一覧を作成日時の降順で取得
+        $microposts = $user->microposts()->orderBy('created_at', 'desc')->paginate(10);
+        
         return view('users.show',[
             'user'=>$user,
+            'microposts' => $microposts,
         ]);
     }
     
